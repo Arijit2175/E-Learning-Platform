c4b2459d6cc006cca8654f515cddf8c3943e5d71
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-12-26 19:56:51 +0530

    cors middleware

--- elearn-backend/app/main.py
+++ elearn-backend/app/main.py
@@ -1,7 +1,19 @@
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import auth, courses, enrollments, assignments, lessons, attendance, quizzes, resources, certificates, ai_tutor_chats, class_schedules, contact_messages
 
+
 app = FastAPI()
+
+# CORS setup for frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(courses.router)
